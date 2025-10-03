54b7aabfedf340629a4fe6a3ec3e9725c723234b
lalima03 <lalimadutta03@gmail.com>
2025-10-03 23:49:24 +0530

    Add MoveZeroes solution with .gitignore

---
+++ arrays/MoveZeroes.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int writer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[writer] = nums[i];
+                writer++;
+            }
+        }
+        for (int i = writer; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
